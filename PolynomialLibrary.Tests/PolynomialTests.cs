e95d87100f7ae5166c625f2bc5b3637bdaabbb0d
Ivan <ivan.trashchenko@gmail.com>
2018-07-10 03:44:35 +0300

    Add Task 2

---
+++ PolynomialLibrary.Tests/PolynomialTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PolynomialLibrary.Tests
+{
+    [TestFixture]
+    public class PolynomialTests
+    {
+        [TestCase(1, 2, 3, 4, ExpectedResult = "x^0 + 2x^1 + 3x^2 + 4x^3")]
+        [TestCase(new double[] { 5, 0, 9, 45 }, ExpectedResult = "5x^0 + 9x^2 + 45x^3")]
+        [TestCase(16, 25, 8, 0, 34, 7.3, 2334556, 12, 0, 0, 0, 1, 0, 0, 0, ExpectedResult = "16x^0 + 25x^1 + 8x^2 + 34x^4 + 7,3x^5 + 2334556x^6 + 12x^7 + x^11")]
+        [TestCase(0d, ExpectedResult = "0")]
+        public string Polynomial_DoubleParams_ToString_Test(params double[] coefficients)
+        {
+            Polynomial polynomial = new Polynomial(coefficients);
+
+            return polynomial.ToString();
+        }
+
+        [TestCase(null)]
+        public void Polynomial_DoubleParams_ThrowsArgumentNullException(double[] coefficients)
+        {
+            Assert.Throws<ArgumentNullException>(() => new Polynomial(coefficients));
+        }
+
+        [TestCase(0, ExpectedResult = "x^0")]
+        [TestCase(5, ExpectedResult = "x^0 + x^1 + x^2 + x^3 + x^4 + x^5")]
+        public string Polynomial_IntDegree_ToString_Test(int degree)
+        {
+            Polynomial polynomial = new Polynomial(degree);
+
+            return polynomial.ToString();
+        }
+
+        [TestCase(-2)]
+        public void Polynomial_IntDegree_ThrowsArgumentOutOfRangeException(int degree)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Polynomial(degree));
+        }
+       
+        [TestCase(new double[] { 1, 3, 7, 9, -3, 8.32546, 1234, 0, 0, 3, 5, 7, 1 },
+                  new double[] { 7, 3, 5, 9, 5.9, 2, 0, 32, 4567, 23, 09, 0d, 0, 0, 0, 4, 0 },
+            ExpectedResult = "8x^0 + 6x^1 + 12x^2 + 18x^3 + 2,9x^4 + 10,32546x^5 + 1234x^6 + 32x^7 + 4567x^8 + 26x^9 + 14x^10 + 7x^11 + x^12 + 4x^15")]       
+        public string Polynomial_Addition_Test(double[] leftCoeffs, double[] rightCoeffs)
+        {
+            Polynomial a = new Polynomial(leftCoeffs);
+            Polynomial b = new Polynomial(rightCoeffs);
+
+            return (a + b).ToString();
+        }
+
+        [TestCase(new double[] { 1, 3, 7, 9, -3, 8.32546, 1234, 0, 0, 3, 5, 7, 1 },
+                  new double[] { 7, 3, 5, 9, 5.9, 2, 0, 32, 4567, 23, 09, 0d, 0, 0, 0, 4, 0 },
+            ExpectedResult = "-6x^0 + 2x^2 + -8,9x^4 + 6,32546x^5 + 1234x^6 + -32x^7 + -4567x^8 + -20x^9 + -4x^10 + 7x^11 + x^12 + -4x^15")]
+        public string Polynomial_Subtraction_Test(double[] leftCoeffs, double[] rightCoeffs)
+        {
+            Polynomial a = new Polynomial(leftCoeffs);
+            Polynomial b = new Polynomial(rightCoeffs);
+
+            return (a - b).ToString();
+        }
+
+        [TestCase(new double[] { 1, 3, 7, 9, -3, 8.32546 },
+            new double[] { 7, 3, 9, 09, 0d, 0, 0, 0, 4, 0 },
+            ExpectedResult = "7x^0 + 24x^1 + 67x^2 + 120x^3 + 96x^4 + 193,27822x^5 + 78,97638x^6 + 47,92914x^7 + 78,92914x^8 + 12x^9 + 28x^10 + 36x^11 + -12x^12 + 33,30184x^13")]
+        public string Polynomial_Multiplication_Test(double[] leftCoeffs, double[] rightCoeffs)
+        {
+            Polynomial a = new Polynomial(leftCoeffs);
+            Polynomial b = new Polynomial(rightCoeffs);
+
+            return (a * b).ToString();
+        }
+
+        [TestCase(new double[] { 1, 2, 3 }, new double[] { 1, 2, 3 }, ExpectedResult = true)]
+        [TestCase(new double[] { 4, 8, 5.8, 0, 12, 0, 0, 0 }, new double[] { 4, 8, 5.8, 0, 12 }, ExpectedResult = true)]
+        [TestCase(new double[] { 1, 2 }, new double[] { 1, 2, 3 }, ExpectedResult = false)]
+        public bool Polynomial_Equals_Test(double[] leftCoeffs, double[] rightCoeffs)
+        {
+            Polynomial a = new Polynomial(leftCoeffs);
+            Polynomial b = new Polynomial(rightCoeffs);
+
+            return a.Equals(b);
+        }
+
+        [Test]
+        public void Polynomial_GetHashCode_Test()
+        {
+            Polynomial a = new Polynomial(1, 2, 3, 4);
+            Polynomial b = new Polynomial(1, 2, 3, 4);
+            Polynomial c = new Polynomial(1, 2, 3);
+
+            Assert.AreEqual(a.GetHashCode(), b.GetHashCode());
+            Assert.AreNotEqual(a.GetHashCode(), c.GetHashCode());
+        }
+    }
+}
---
+++ PolynomialLibrary/Polynomial.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace PolynomialLibrary
+{
+    /// <summary>
+    /// Immutable polynomial class.
+    /// </summary>
+    public sealed class Polynomial : ICloneable, IEquatable<Polynomial>
+    {
+
+        #region Fields
+
+        /// <summary>
+        /// The precision of calculations when comparing two variables of the double type.
+        /// </summary>
+        private static readonly double PRECISION;
+
+        /// <summary>
+        /// Coefficients of polynomial.
+        /// </summary>
+        private readonly double[] coefficients;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Static constructor for precision parsing from app.config.
+        /// </summary>
+        static Polynomial()
+        {
+            try
+            {
+                PRECISION = double.Parse(ConfigurationManager.AppSettings["precision"]);
+
+            }
+            catch (ArgumentNullException)
+            {
+                PRECISION = 0.0000001;
+            }
+        }
+
+        /// <summary>
+        /// Constructs a new polynomial of degree equal to 0.
+        /// </summary>
+        public Polynomial()
+            : this(0)
+        {
+        }
+
+        /// <summary>
+        /// Constructs a new polynomial of the specified degree.
+        /// </summary>
+        /// <param name="degree">Degree of polynomial.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when degree value is negative.</exception>
+        public Polynomial(int degree)
+        {
+            if (degree < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(degree)} can not be negative.");
+            }
+
+            this.Degree = degree;
+            this.coefficients = new double[degree + 1];
+
+            List<double> coeffsList = new List<double>();
+
+            for (int i = 0; i <= degree; i++)
+            {
+                coeffsList.Add(1.0);
+            }
+
+            Array.Copy(coeffsList.ToArray(), this.coefficients, coeffsList.ToArray().Length);
+        }
+
+        /// <summary>
+        /// Constructs a new polynomial with the specified coefficients.
+        /// </summary>
+        /// <param name="coefficients">Coefficients of polynomial.</param>
+        /// <exception cref="ArgumentNullException">Thrown when coefficients array is equal to null.</exception>
+        /// <exception cref="ArgumentException">Thrown when coefficients array is empty.</exception>
+        public Polynomial(params double[] coefficients)
+        {
+            if (coefficients == null)
+            {
+                throw new ArgumentNullException($"{nameof(coefficients)} can not be equal to null.");
+            }
+
+            if (coefficients.Length == 0)
+            {
+                throw new ArgumentException($"{nameof(coefficients)} can not be empty.");
+            }
+
+            Stack<double> coeffStack = new Stack<double>(coefficients);
+
+            for (int i = coefficients.Length - 1; i > 0; i--)
+            {
+                if (Math.Abs(coefficients[i]) < PRECISION)
+                {
+                    coeffStack.Pop();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            this.Degree = coeffStack.Count - 1;
+
+            this.coefficients = new double[coeffStack.Count];
+
+            Array.Copy(coeffStack.Reverse().ToArray(), this.coefficients, coeffStack.Count);
+        }
+
+        #endregion
+
+        #region Public methods + properties
+
+        /// <summary>
+        /// Gets the degree of polynomial.
+        /// </summary>
+        public int Degree { get; }
+
+        /// <summary>
+        /// Returns copy of polymomial's coefficients array.
+        /// </summary>
+        /// <returns>Array of <see cref="double"/> type.</returns>
+        public double[] ToArray()
+        {
+            var copyArray = new double[this.Degree + 1];
+
+            Array.Copy(this.coefficients, copyArray, this.Degree + 1);
+
+            return copyArray;
+        }
+
+        /// <summary>
+        /// Indexer of <see cref="Polynomial"/> class.
+        /// </summary>
+        /// <param name="index">Current index.</param>       
+        /// <returns>Coefficient of polynomial at the specified index.</returns>
+        /// <exception cref="IndexOutOfRangeException">Throws when index is out of range.</exception>
+        public double this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > this.Degree)
+                {
+                    throw new IndexOutOfRangeException($"{nameof(index)} is out of range.");
+                }
+
+                return this.coefficients[index];
+            }
+        }
+
+        #endregion
+
+        #region Object overrided methods
+
+        public override string ToString()
+        {
+            if (this.Degree == 0 && Math.Abs(this[0]) < PRECISION)
+            {
+                return "0";
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i <= this.Degree; i++)
+            {
+                if (Math.Abs(this[i]) < PRECISION)
+                {
+                    continue;
+                }
+
+                if (Math.Abs(this[i] - 1) < PRECISION)
+                {
+                    if (i == this.Degree)
+                    {
+                        sb.Append("x^" + i);
+                    }
+                    else
+                    {
+                        sb.Append("x^" + i + " + ");
+                    }
+                }
+                else
+                {
+                    if (i == this.Degree)
+                    {
+                        sb.Append(this[i] + "x^" + i);
+                    }
+                    else
+                    {
+                        sb.Append(this[i] + "x^" + i + " + ");
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public override bool Equals(object other)
+        {
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(other, null))
+            {
+                return false;
+            }
+
+            if (this.GetType() != other.GetType())
+            {
+                return false;
+            }
+
+            Polynomial polynomial = (Polynomial)other;
+
+            return this.Equals(polynomial);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashResult = 397;
+
+                foreach (var item in this.coefficients)
+                {
+                    hashResult += item.GetHashCode();
+                }
+
+                return hashResult ^ this.Degree;
+            }
+        }
+
+        #endregion
+
+        #region Operator methods
+
+        public static Polynomial Add(Polynomial lhs, Polynomial rhs)
+        {
+            if (lhs == null)
+            {
+                throw new ArgumentNullException($"{nameof(lhs)} can not be null.");
+            }
+
+            if (rhs == null)
+            {
+                throw new ArgumentNullException($"{nameof(rhs)} can not be null.");
+            }
+
+            int maxDegree = (lhs.Degree > rhs.Degree) ? lhs.Degree : rhs.Degree;
+            var resultCoeffs = new double[maxDegree + 1];
+            Array.Copy(lhs.ToArray(), resultCoeffs, lhs.Degree + 1);
+
+            for (int i = 0; i <= rhs.Degree; i++)
+            {
+                resultCoeffs[i] += rhs[i];
+            }
+
+            return new Polynomial(resultCoeffs);
+        }
+
+        public static Polynomial Substract(Polynomial lhs, Polynomial rhs)
+        {
+            if (lhs == null)
+            {
+                throw new ArgumentNullException($"{nameof(lhs)} can not be null.");
+            }
+
+            if (rhs == null)
+            {
+                throw new ArgumentNullException($"{nameof(rhs)} can not be null.");
+            }
+
+            int maxDegree = (lhs.Degree > rhs.Degree) ? lhs.Degree : rhs.Degree;
+
+            var resultCoeffs = new double[maxDegree + 1];
+            Array.Copy(lhs.ToArray(), resultCoeffs, lhs.Degree + 1);
+
+            for (int i = 0; i <= rhs.Degree; i++)
+            {
+                resultCoeffs[i] -= rhs[i];
+            }
+
+            return new Polynomial(resultCoeffs);
+        }
+
+        public static Polynomial Multiply(Polynomial lhs, Polynomial rhs)
+        {
+
+            if (lhs == null)
+            {
+                throw new ArgumentNullException($"{nameof(lhs)} can not be null.");
+            }
+
+            if (rhs == null)
+            {
+                throw new ArgumentNullException($"{nameof(rhs)} can not be null.");
+            }
+
+            var resultCoeffs = new double[lhs.Degree + rhs.Degree + 2];
+
+            for (int i = 0; i <= lhs.Degree; i++)
+            {
+                for (int j = 0; j <= rhs.Degree; j++)
+                {
+                    resultCoeffs[i + j] += lhs[i] * rhs[j];
+                }
+            }
+
+            return new Polynomial(resultCoeffs);
+        }
+
+        #endregion
+
+        #region Overloaded operators
+
+        public static Polynomial operator +(Polynomial lhs, Polynomial rhs) => Add(lhs, rhs);
+
+        public static bool operator ==(Polynomial lhs, Polynomial rhs) => lhs.Equals(rhs);
+
+        public static bool operator !=(Polynomial lhs, Polynomial rhs) => !lhs.Equals(rhs);
+
+        public static Polynomial operator *(Polynomial lhs, Polynomial rhs) => Multiply(lhs, rhs);
+
+        public static Polynomial operator -(Polynomial lhs, Polynomial rhs) => Substract(lhs, rhs);
+
+        #endregion
+
+        #region Interface overrided methods
+
+        public bool Equals(Polynomial other)
+        {
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(other, null))
+            {
+                return false;
+            }
+
+            if (this.Degree != other.Degree)
+            {
+                return false;
+            }
+
+            for (int i = 0; i <= this.Degree; i++)
+            {
+                if (Math.Abs(this[i] - other[i]) > PRECISION)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        object ICloneable.Clone() => this.Clone();
+
+        public Polynomial Clone() => new Polynomial(this.coefficients);
+
+        #endregion
+
+    }
+}
